7ba6e290ae7f9d4518144f7d2d46518a302138b7
Earther <qn.khuat@gmail.com>
2021-01-07 17:26:17 +0700

    let's see if the problem is bc of arm chim

--- KaleidoscopeJIT.h
+++ KaleidoscopeJIT.h
@@ -13,91 +13,129 @@
 #ifndef LLVM_EXECUTIONENGINE_ORC_KALEIDOSCOPEJIT_H
 #define LLVM_EXECUTIONENGINE_ORC_KALEIDOSCOPEJIT_H
 
-#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/iterator_range.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
 #include "llvm/ExecutionEngine/JITSymbol.h"
 #include "llvm/ExecutionEngine/Orc/CompileUtils.h"
-#include "llvm/ExecutionEngine/Orc/Core.h"
-#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
 #include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
-#include "llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h"
+#include "llvm/ExecutionEngine/Orc/LambdaResolver.h"
 #include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
-#include "llvm/ExecutionEngine/Orc/TargetProcessControl.h"
+#include "llvm/ExecutionEngine/RTDyldMemoryManager.h"
 #include "llvm/ExecutionEngine/SectionMemoryManager.h"
 #include "llvm/IR/DataLayout.h"
-#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Mangler.h"
+#include "llvm/Support/DynamicLibrary.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+#include <algorithm>
+#include <map>
 #include <memory>
+#include <string>
+#include <vector>
 
 namespace llvm {
-	namespace orc {
+namespace orc {
 
-		class KaleidoscopeJIT {
-			private:
-				std::unique_ptr<TargetProcessControl> TPC;
-				std::unique_ptr<ExecutionSession> ES;
+class KaleidoscopeJIT {
+public:
+  using ObjLayerT = LegacyRTDyldObjectLinkingLayer;
+  using CompileLayerT = LegacyIRCompileLayer<ObjLayerT, SimpleCompiler>;
 
-				DataLayout DL;
-				MangleAndInterner Mangle;
+  KaleidoscopeJIT()
+      : Resolver(createLegacyLookupResolver(
+            ES,
+            [this](StringRef Name) {
+              return findMangledSymbol(std::string(Name));
+            },
+            [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); })),
+        TM(EngineBuilder().selectTarget()), DL(TM->createDataLayout()),
+        ObjectLayer(AcknowledgeORCv1Deprecation, ES,
+                    [this](VModuleKey) {
+                      return ObjLayerT::Resources{
+                          std::make_shared<SectionMemoryManager>(), Resolver};
+                    }),
+        CompileLayer(AcknowledgeORCv1Deprecation, ObjectLayer,
+                     SimpleCompiler(*TM)) {
+    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+  }
 
-				RTDyldObjectLinkingLayer ObjectLayer;
-				IRCompileLayer CompileLayer;
+  TargetMachine &getTargetMachine() { return *TM; }
 
-				JITDylib &MainJD;
+  VModuleKey addModule(std::unique_ptr<Module> M) {
+    auto K = ES.allocateVModule();
+    cantFail(CompileLayer.addModule(K, std::move(M)));
+    ModuleKeys.push_back(K);
+    return K;
+  }
 
-			public:
-				KaleidoscopeJIT(std::unique_ptr<TargetProcessControl> TPC,
-						std::unique_ptr<ExecutionSession> ES,
-						JITTargetMachineBuilder JTMB, DataLayout DL)
-					: TPC(std::move(TPC)), ES(std::move(ES)), DL(std::move(DL)),
-					Mangle(*this->ES, this->DL),
-					ObjectLayer(*this->ES,
-							[]() { return std::make_unique<SectionMemoryManager>(); }),
-					CompileLayer(*this->ES, ObjectLayer,
-							std::make_unique<ConcurrentIRCompiler>(std::move(JTMB))),
-					MainJD(this->ES->createBareJITDylib("<main>")) {
-						MainJD.addGenerator(
-								cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
-										DL.getGlobalPrefix())));
-					}
+  void removeModule(VModuleKey K) {
+    ModuleKeys.erase(find(ModuleKeys, K));
+    cantFail(CompileLayer.removeModule(K));
+  }
 
-				~KaleidoscopeJIT() {
-					if (auto Err = ES->endSession())
-						ES->reportError(std::move(Err));
-				}
+  JITSymbol findSymbol(const std::string Name) {
+    return findMangledSymbol(mangle(Name));
+  }
 
-				static Expected<std::unique_ptr<KaleidoscopeJIT>> Create() {
-					auto SSP = std::make_shared<SymbolStringPool>();
-					auto TPC = SelfTargetProcessControl::Create(SSP);
-					if (!TPC)
-						return TPC.takeError();
+private:
+  std::string mangle(const std::string &Name) {
+    std::string MangledName;
+    {
+      raw_string_ostream MangledNameStream(MangledName);
+      Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
+    }
+    return MangledName;
+  }
 
-					auto ES = std::make_unique<ExecutionSession>(std::move(SSP));
+  JITSymbol findMangledSymbol(const std::string &Name) {
+#ifdef _WIN32
+    // The symbol lookup of ObjectLinkingLayer uses the SymbolRef::SF_Exported
+    // flag to decide whether a symbol will be visible or not, when we call
+    // IRCompileLayer::findSymbolIn with ExportedSymbolsOnly set to true.
+    //
+    // But for Windows COFF objects, this flag is currently never set.
+    // For a potential solution see: https://reviews.llvm.org/rL258665
+    // For now, we allow non-exported symbols on Windows as a workaround.
+    const bool ExportedSymbolsOnly = false;
+#else
+    const bool ExportedSymbolsOnly = true;
+#endif
 
-					JITTargetMachineBuilder JTMB((*TPC)->getTargetTriple());
+    // Search modules in reverse order: from last added to first added.
+    // This is the opposite of the usual search order for dlsym, but makes more
+    // sense in a REPL where we want to bind to the newest available definition.
+    for (auto H : make_range(ModuleKeys.rbegin(), ModuleKeys.rend()))
+      if (auto Sym = CompileLayer.findSymbolIn(H, Name, ExportedSymbolsOnly))
+        return Sym;
 
-					auto DL = JTMB.getDefaultDataLayoutForTarget();
-					if (!DL)
-						return DL.takeError();
+    // If we can't find the symbol in the JIT, try looking in the host process.
+    if (auto SymAddr = RTDyldMemoryManager::getSymbolAddressInProcess(Name))
+      return JITSymbol(SymAddr, JITSymbolFlags::Exported);
 
-					return std::make_unique<KaleidoscopeJIT>(std::move(*TPC), std::move(ES),
-							std::move(JTMB), std::move(*DL));
-				}
+#ifdef _WIN32
+    // For Windows retry without "_" at beginning, as RTDyldMemoryManager uses
+    // GetProcAddress and standard libraries like msvcrt.dll use names
+    // with and without "_" (for example "_itoa" but "sin").
+    if (Name.length() > 2 && Name[0] == '_')
+      if (auto SymAddr =
+              RTDyldMemoryManager::getSymbolAddressInProcess(Name.substr(1)))
+        return JITSymbol(SymAddr, JITSymbolFlags::Exported);
+#endif
 
-				const DataLayout &getDataLayout() const { return DL; }
+    return nullptr;
+  }
 
-				JITDylib &getMainJITDylib() { return MainJD; }
+  ExecutionSession ES;
+  std::shared_ptr<SymbolResolver> Resolver;
+  std::unique_ptr<TargetMachine> TM;
+  const DataLayout DL;
+  ObjLayerT ObjectLayer;
+  CompileLayerT CompileLayer;
+  std::vector<VModuleKey> ModuleKeys;
+};
 
-				Error addModule(ThreadSafeModule TSM, ResourceTrackerSP RT = nullptr) {
-					if (!RT)
-						RT = MainJD.getDefaultResourceTracker();
-					return CompileLayer.add(RT, std::move(TSM));
-				}
-
-				Expected<JITEvaluatedSymbol> lookup(StringRef Name) {
-					return ES->lookup({&MainJD}, Mangle(Name.str()));
-				}
-		};
-
-	} // end namespace orc
+} // end namespace orc
 } // end namespace llvm
 
 #endif // LLVM_EXECUTIONENGINE_ORC_KALEIDOSCOPEJIT_H
--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 # Run
-clang++ -g -O3 part.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o lingua
+#clang++ -g -O3 part.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o lingua 
+clang++ -g part.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core mcjit native` -O3 -o lingua -v
+#clang++ -g part.cpp  \
+#    -I/Users/earther/fun/1_lingua/llvm-project/llvm/include -I/Users/earther/fun/1_lingua/llvm-project/build/include -std=c++14   -fno-exceptions -fno-rtti -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS \
+#    -L/Users/earther/fun/1_lingua/llvm-project/build/Debug/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names \
+#    -lLLVMCore -lLLVMRemarks -lLLVMBitstreamReader -lLLVMBinaryFormat -lLLVMSupport -lLLVMDemangle \
+#    -lm -lz -lcurses -lxml2 \
+#    -o lingua -v
+
 if [ $? -eq 0 ]; then ./lingua; fi
+
